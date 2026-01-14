3095158bb9202de7ef2a4310440b2f72fb44fd98
sty20030818 <544760043@qq.com>
2026-01-14 16:07:01 +0800

    feat: 备案 和临时修改网站名

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "石头鱼的博客",
+	title: "这是 sty 啦",
 	subtitle: "欢迎",
 	siteURL: "https://www.sty20030818.space", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-08-18", // 站点开始运行日期，用于站点统计组件计算运行天数
@@ -51,7 +51,7 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 顶栏标题文本
-		text: "石头鱼的博客",
+		text: "这是 sty 啦",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 	},
@@ -140,7 +140,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "石头鱼的博客", // 主页横幅主标题
+			title: "这是 sty 啦", // 主页横幅主标题
 
 			subtitle: [
 				"一个小小的 喜欢 UI/UX 的全栈工程师",
@@ -466,8 +466,9 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 };
 
 export const footerConfig: FooterConfig = {
-	enable: false, // 是否启用Footer HTML注入功能
-	customHtml: "", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	enable: true, // 是否启用Footer HTML注入功能
+	customHtml:
+		"<p>备案号：<a href='https://beian.miit.gov.cn/'>浙ICP备2023036258号</a></p>", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
